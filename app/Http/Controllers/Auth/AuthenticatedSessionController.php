8127fdf595b4b6429d84c48bf51c52111b00aa97
Lucas Lourenço <lucaspeixe2018@gmail.com>
2022-09-21 10:55:25 -0300

    listner

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -35,7 +35,6 @@ class AuthenticatedSessionController extends Controller
         Auth::guard('web')->logout();
 
         //$request->session()->invalidate();
-
         //$request->session()->regenerateToken();
 
         return redirect('login');
