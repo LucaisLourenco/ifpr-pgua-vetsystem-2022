358b6d0c1dc86c1c72d3164c44c09ab1b5507c69
Lucas Lourenço <lucaspeixe2018@gmail.com>
2022-09-21 11:04:44 -0300

    listner

--- app/Policies/RacaPolicy.php
+++ app/Policies/RacaPolicy.php
@@ -11,83 +11,36 @@ class RacaPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function viewAny(User $user)
     {
-        //
+        return UserPermissions::isAuthorized('racas.index');
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Raca  $raca
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function view(User $user, Raca $raca)
     {
-        //
+        return UserPermissions::isAuthorized('racas.index');
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function create(User $user)
     {
-        //
+        return UserPermissions::isAuthorized('racas.create');
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Raca  $raca
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function update(User $user, Raca $raca)
     {
-        //
+        return UserPermissions::isAuthorized('racas.edit');
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Raca  $raca
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function delete(User $user, Raca $raca)
     {
-        //
+        return UserPermissions::isAuthorized('racas.destroy');
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Raca  $raca
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function restore(User $user, Raca $raca)
     {
         //
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Raca  $raca
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function forceDelete(User $user, Raca $raca)
     {
         //
