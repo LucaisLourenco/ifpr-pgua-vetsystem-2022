7d69845d04ed9e5c0191a79013a38157d42ff40a
Lucas Lourenço <lucaspeixe2018@gmail.com>
2022-09-26 21:12:08 -0300

    add-telefones-clientes

--- app/Http/Controllers/ClienteTelefoneController.php
+++ app/Http/Controllers/ClienteTelefoneController.php
@@ -2,84 +2,101 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cliente;
 use App\Models\ClienteTelefone;
 use Illuminate\Http\Request;
 
+$GLOBALS['regras'] = [
+    'nome_telefone' => 'required|min:3|max:30',
+    'numero_telefone' => 'required|min:14',
+];
+
+$GLOBALS['mensagem']= [
+    "nome_telefone.required" => "O preenchimento do campo Nome do Contato é obrigatório!",
+    "nome_telefone.max" => "O campo Nome do Contato possui tamanho máxixo de 30 caracteres!",
+    "nome_telefone.min" => "O campo Nome do Contato possui tamanho mínimo de 2 caracteres!",
+    "numero_telefone.required" => "O preenchimento do campo Contato é obrigatório!",
+    "numero_telefone.min" => "O campo Contato possui tamanho mínimo de 10 dígitos!",
+];
+
 class ClienteTelefoneController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+{ 
     public function index()
     {
         //
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+ 
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    public function newTelefone($cliente) {
+        return view('clienteTelefones.newTelefone', compact(['cliente']));
+    }
+ 
     public function store(Request $request)
     {
-        //
-    }
+        $request->validate($GLOBALS['regras'],$GLOBALS['mensagem']);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\ClienteTelefone  $clienteTelefone
-     * @return \Illuminate\Http\Response
-     */
+        $cliente = Cliente::find($request->cliente_id);
+
+        $telefone = new ClienteTelefone();
+        $telefone->nome = mb_strtoupper($request->nome_telefone);
+        $telefone->numero = $request->numero_telefone;
+        $telefone->cliente()->associate($cliente);
+        $telefone->save();
+
+        return redirect()->to('clientes/'.$cliente->id);
+    }
+ 
     public function show(ClienteTelefone $clienteTelefone)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\ClienteTelefone  $clienteTelefone
-     * @return \Illuminate\Http\Response
-     */
     public function edit(ClienteTelefone $clienteTelefone)
     {
-        //
+        return view('clienteTelefones.edit', compact(['clienteTelefone']));
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\ClienteTelefone  $clienteTelefone
-     * @return \Illuminate\Http\Response
-     */
+ 
     public function update(Request $request, ClienteTelefone $clienteTelefone)
     {
-        //
-    }
+        $request->validate($GLOBALS['regras'],$GLOBALS['mensagem']);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\ClienteTelefone  $clienteTelefone
-     * @return \Illuminate\Http\Response
-     */
+        try
+        {
+            $clienteTelefone->update([
+                "nome" => mb_strtoupper($request->nome_telefone),
+                "numero" => $request->numero_telefone,
+            ]);
+
+            session()->flash('mensagem', "Item alterado com sucesso.");
+            session()->flash('resultado', true);
+
+        } catch(\Exception $exception) 
+        {
+            session()->flash('mensagem', $exception->getMessage());
+            session()->flash('resultado', null);
+        }
+
+        return redirect()->to('clientes/'.$clienteTelefone->cliente_id);
+    }
+ 
     public function destroy(ClienteTelefone $clienteTelefone)
     {
-        //
+        try
+        {
+            $clienteTelefone->delete();
+            session()->flash('mensagem', "Item excluído com sucesso.");
+            session()->flash('resultado', true);
+           
+        } catch(\Exception $exception)
+        { 
+            session()->flash('mensagem', $exception->getMessage());
+            session()->flash('resultado', null);
+        }
+
+        return redirect()->to('clientes/'.$clienteTelefone->cliente_id);
     }
 }
--- resources/views/clientes/show.blade.php
+++ resources/views/clientes/show.blade.php
@@ -44,41 +44,12 @@
                     @foreach ($cliente->pets as $item)
                         <tr>
                             <td>{{ $item['nome'] }}</td>
-                            <td>{{ $item['nome'] }}</td>
-                            <td>{{ $item['nome'] }}</td>
-                            <td>{{ $item['nome'] }}</td>
-                            <td>
-                                <a href= "" class="btn btn-success">                                    
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
-                                        <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/>
-                                    </svg>
-                                </a>
-
-                                <!--<a nohref style="cursor:pointer" onclick="showInfoModalEndereco('{{ $item['nome'] }}','{{ $item['cep'] }}','{{ $item['rua'] }}',
-                                        '{{ $item['numero'] }}','{{ $item['complemento'] }}','{{ $item['bairro'] }}','{{ $item['cidade'] }}',
-                                            '{{ $item['uf'] }}')" class="btn btn-primary">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
-                                        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-                                    </svg>
-                                </a>-->
-
-                                <!--<a nohref style="cursor:pointer" onclick="showRemoveModalEndereco('{{ $item['id'] }}', '{{ $item['nome'] }}')" class="btn btn-danger">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                                    </svg>
-                                </a>
-                            </td>
-                            <form action="" method="POST" id="pet_{{$item['id']}}">
-                                @csrf
-                                @method('DELETE')
-                            </form>-->
                         </tr>
                     @endforeach
                 </tbody>
             </table>
             <div class="col d-flex justify-content-end">
-                <a href= "" class="btn btn-secondary text-white" data-bs-toggle="modal" data-bs-target="#createModalEndereco">
+                <a href="" class="btn btn-secondary text-white" data-bs-toggle="modal" data-bs-target="#createModalEndereco">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
                     </svg>
@@ -88,7 +59,11 @@
     </div>
     
     <hr>
-    <div class="row">    
+
+    <!--TABELA DE ENDERECOS E TELEFONES-->
+    <div class="row">  
+        
+        <!--ENDERECOS-->
         <div class="col-6">   
             <table class="table align-middle caption-top table-striped">
                 <caption>Lista de <b>Endereços</b></caption>
@@ -107,6 +82,7 @@
                             <td>{{ $item['rua'] }}</td>
                             <td>{{ $item['bairro'] }}</td>
                             <td>
+                                <!--ENDERECOS EDIT-->
                                 <a href= "{{ route('clienteEnderecos.edit', $item) }}" class="btn btn-success">                                    
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
@@ -114,7 +90,8 @@
                                     </svg>
                                 </a>
 
-                                <a nohref style="cursor:pointer" onclick="showInfoModalEndereco('{{ $item['nome'] }}','{{ $item['cep'] }}','{{ $item['rua'] }}',
+                                <!--ENDERECOS SHOW-->
+                                <a nohref onclick="showInfoModalEndereco('{{ $item['nome'] }}','{{ $item['cep'] }}','{{ $item['rua'] }}',
                                         '{{ $item['numero'] }}','{{ $item['complemento'] }}','{{ $item['bairro'] }}','{{ $item['cidade'] }}',
                                             '{{ $item['uf'] }}')" class="btn btn-primary">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
@@ -122,7 +99,8 @@
                                     </svg>
                                 </a>
 
-                                <a nohref style="cursor:pointer" onclick="showRemoveModalEndereco('{{ $item['id'] }}', '{{ $item['nome'] }}')" class="btn btn-danger">
+                                <!--ENDERECOS DESTROY-->
+                                <a nohref onclick="showRemoveModalEndereco('{{ $item['id'] }}', '{{ $item['nome'] }}')" class="btn btn-danger">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                         <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                                     </svg>
@@ -136,14 +114,18 @@
                     @endforeach
                 </tbody>
             </table>
+
+            <!--ENDERECOS CADASTRAR-->
             <div class="col d-flex justify-content-end">
-                <a href= "{{ route('clienteEnderecos.newEndereco', $cliente) }}" class="btn btn-secondary">
+                <a href="{{ route('clienteEnderecos.newEndereco', $cliente) }}" class="btn btn-secondary">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
                     </svg>
                 </a>
             </div> 
         </div>
+
+        <!--TELEFONES-->
         <div class="col-6">    
             <table class="table align-middle caption-top table-striped">
                 <caption>Lista de <b>Telefones</b></caption>
@@ -160,19 +142,22 @@
                             <td>{{ $item['nome'] }}</td>
                             <td>{{ $item['numero'] }}</td>
                             <td>
-                                <a href= "#" class="btn btn-success">
+                                <!--TELEFONES EDIT-->
+                                <a href="{{ route('clienteTelefones.edit', $item) }}" class="btn btn-success">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
                                         <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/>
                                     </svg>
                                 </a>
-                                <a nohref style="cursor:pointer" onclick="showRemoveModalTelefone('{{ $item['id'] }}', '{{ $item['numero'] }}')" class="btn btn-danger">
+
+                                <!--TELEFONES DESTROY-->
+                                <a nohref onclick="showRemoveModalTelefone('{{ $item['id'] }}', '{{ $item['numero'] }}')" class="btn btn-danger">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                         <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                                     </svg>
                                 </a>
                             </td>
-                            <form action="" method="POST" id="telefone_{{$item['id']}}">
+                            <form action="{{ route('clienteTelefones.destroy', $item['id']) }}" method="POST" id="telefone_{{$item['id']}}">
                                 @csrf
                                 @method('DELETE')
                             </form>
@@ -180,8 +165,10 @@
                     @endforeach
                 </tbody>
             </table>
+
+            <!--TELEFONES CADASTRAR-->
             <div class="col d-flex justify-content-end">
-                <a href= "" class="btn btn-secondary text-white">
+                <a href="{{ route('clienteTelefones.newTelefone', $cliente) }}" class="btn btn-secondary text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
                     </svg>
