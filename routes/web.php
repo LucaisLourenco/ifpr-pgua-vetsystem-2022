ee72f4f0751c6070a090f72193e4a7afbc113078
Lucas Lourenço <lucaspeixe2018@gmail.com>
2022-09-29 13:03:21 -0300

    add-prefix-sistema

--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,28 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteEnderecoController;
 use App\Http\Controllers\ClienteTelefoneController;
 
-Route::resource('parametros', 'ParametroController')->middleware(['auth','verified']);
+Route::group(['middleware' => ['auth','verified'], 'prefix' => 'sistema'], function() {
 
-Route::resource('generos', 'GeneroController')->middleware(['auth','verified']);
+    Route::resource('parametros', 'ParametroController');
 
-Route::resource('especies', 'EspecieController')->middleware(['auth','verified']);
+    Route::resource('generos', 'GeneroController');
 
-Route::resource('racas', 'RacaController')->middleware(['auth','verified']);
+    Route::resource('especies', 'EspecieController');
 
-Route::resource('clientes', 'ClienteController')->middleware(['auth','verified']);
+    Route::resource('racas', 'RacaController');
 
-Route::resource('clienteEnderecos', 'ClienteEnderecoController')->middleware(['auth','verified'])
-    ->middleware(['auth','verified']);
+    Route::resource('clientes', 'ClienteController');
 
-Route::get('/newEnderecoCliente/{cliente}', [ClienteEnderecoController::class, 'newEndereco'])
-    ->name('clienteEnderecos.newEndereco')->middleware(['auth','verified']);
+    Route::resource('clienteEnderecos', 'ClienteEnderecoController');
 
-Route::resource('clienteTelefones', 'ClienteTelefoneController')->middleware(['auth','verified'])
-    ->middleware(['auth','verified']);
+    Route::get('/newEnderecoCliente/{cliente}', [ClienteEnderecoController::class, 'newEndereco'])
+        ->name('clienteEnderecos.newEndereco');
 
-Route::get('/newTelefoneCliente/{cliente}', [ClienteTelefoneController::class, 'newTelefone'])
-    ->name('clienteTelefones.newTelefone')->middleware(['auth','verified']);
+    Route::resource('clienteTelefones', 'ClienteTelefoneController');
+
+    Route::get('/newTelefoneCliente/{cliente}', [ClienteTelefoneController::class, 'newTelefone'])
+        ->name('clienteTelefones.newTelefone');
+});
 
 Route::get('/sistema', function () {
     return view('templates.main')->with('titulo');
