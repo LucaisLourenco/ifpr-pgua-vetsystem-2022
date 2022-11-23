bf213c162703352d25bebe76ed2d208391101039
Lucas Lourenço <lucaspeixe2018@gmail.com>
2022-11-22 23:29:44 -0300

    add-permissions

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -51,8 +51,15 @@
                                 <span class="ps-1 text-dark">Agendamentos</span>
                             </a>
                             <ul class="dropdown-menu">
+                            
+                            @if(UserPermissions::isAuthorized('consultaagendamentos.index'))
                                 <li><a href="{{ route('consultaagendamentos.index') }}" class="dropdown-item">Gerenciar Consultas</a></li>
+                            @endif
+
+                            @if(UserPermissions::isAuthorized('servicoagendamentos.index'))
                                 <li><a href="{{ route('servicoagendamentos.index') }}" class="dropdown-item">Gerenciar Serviços</a></li>
+                            @endif
+
                             </ul>
                         </li>    
                         <li class="nav-item dropdown ps-1">
@@ -64,24 +71,41 @@
                                 <span class="ps-1 text-dark">Cadastros</span>
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a href="{{ route('pets.index') }}" class="dropdown-item">Manter Pets</a></li>
-                                <li><a href="{{ route('clientes.index') }}" class="dropdown-item">Manter Clientes</a></li>
-                                <li><a href="{{ route('users.index') }}" class="dropdown-item">Manter Funcionário</a></li>
-                                <li><a href="{{ route('veterinarios.index') }}" class="dropdown-item">Manter Veterinários</a></li>
-                            </ul>
-                        </li>               
-                        <li class="nav-item dropdown ps-1">
-                            <a class="nav-link dropdown-toggle text-dark" data-bs-toggle="dropdown" href="#">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-layout-text-sidebar-reverse" viewBox="0 0 16 16">
-                                    <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
-                                    <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
-                                </svg>
-                                <span class="ps-1 text-dark">Administrador
-                            </a>
-                            <ul class="dropdown-menu ">
-                                <li><a href="{{ route('parametros.index') }}" class="dropdown-item">Estabelecimento</a></li>
+
+                                @if(UserPermissions::isAuthorized('pets.index'))
+                                    <li><a href="{{ route('pets.index') }}" class="dropdown-item">Manter Pets</a></li>
+                                @endif
+
+                                @if(UserPermissions::isAuthorized('clientes.index'))
+                                    <li><a href="{{ route('clientes.index') }}" class="dropdown-item">Manter Clientes</a></li>
+                                @endif  
+                                  
+                                @if(UserPermissions::isAuthorized('users.index'))
+                                    <li><a href="{{ route('users.index') }}" class="dropdown-item">Manter Funcionário</a></li>
+                                @endif
+
+                                @if(UserPermissions::isAuthorized('veterinarios.index'))
+                                    <li><a href="{{ route('veterinarios.index') }}" class="dropdown-item">Manter Veterinários</a></li>
+                                @endif
+                            
                             </ul>
                         </li>
+                        
+                        @if(UserPermissions::isAuthorized('parametros.index'))
+                            <li class="nav-item dropdown ps-1">
+                                <a class="nav-link dropdown-toggle text-dark" data-bs-toggle="dropdown" href="#">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-layout-text-sidebar-reverse" viewBox="0 0 16 16">
+                                        <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
+                                        <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
+                                    </svg>
+                                    <span class="ps-1 text-dark">Administrador
+                                </a>
+                                <ul class="dropdown-menu ">
+                                    <li><a href="{{ route('parametros.index') }}" class="dropdown-item">Estabelecimento</a></li>
+                                </ul>
+                            </li>
+                        @endif
+
                         <li class="nav-item dropdown ps-1">
                             <a class="nav-link dropdown-toggle text-dark" data-bs-toggle="dropdown" href="#">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-door-closed-fill" viewBox="0 0 16 16">
@@ -106,15 +130,19 @@
                 <div class="col">
                     <h3 class="display-7 text-secondary d-none d-md-block"><b>{{ $titulo }}</b></h3>
                 </div>
+
                 @if(isset($rota))
-                    <div class="col d-flex justify-content-end">
-                        <a href= "{{ route($rota) }}" class="btn btn-secondary text-white">Cadastrar
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-                            </svg>
-                        </a>
-                    </div>
+                    @if(UserPermissions::isAuthorized($rota))
+                        <div class="col d-flex justify-content-end">
+                            <a href= "{{ route($rota) }}" class="btn btn-secondary text-white">Cadastrar
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#FFF" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                                </svg>
+                            </a>
+                        </div>
+                    @endif
                 @endif
+                
             </div>
             <hr>
                 @yield('conteudo')
