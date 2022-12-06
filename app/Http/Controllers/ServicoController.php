4b8f875ae457b8f61e6074356a94c91212331cdd
Lucas Lourenço <lucaspeixe2018@gmail.com>
2022-12-06 09:58:45 -0300

    edit-servico

--- app/Http/Controllers/ServicoController.php
+++ app/Http/Controllers/ServicoController.php
@@ -6,14 +6,14 @@ use App\Models\Servico;
 use Illuminate\Http\Request;
 
 $GLOBALS['regras'] = [
-    'nome' => 'required|max:30|min:2',
+    'nome' => 'required|max:40|min:2',
     'valor' => 'required',
     'descricao' => 'max:250|min:2',
 ];
 
 $GLOBALS['mensagem']= [
     "nome.required" => "O preenchimento do campo NOME é obrigatório!",
-    "nome.max" => "O campo NOME possui tamanho máxixo de 30 caracteres!",
+    "nome.max" => "O campo NOME possui tamanho máxixo de 40 caracteres!",
     "nome.min" => "O campo NOME possui tamanho mínimo de 2 caracteres!",
     "valor.required" => "O preenchimento do campo Valor é obrigatório!",
     "descricao.max" => "O campo Descrição possui tamanho máxixo de 250 caracteres!",
@@ -74,7 +74,26 @@ class ServicoController extends Controller
 
     public function update(Request $request, Servico $servico)
     {
-        //
+        $request->validate($GLOBALS['regras'],$GLOBALS['mensagem']);
+        
+        try
+        {
+            $servico->update([
+                "nome" => mb_strtoupper($request->nome),
+                "valor" => $request->valor,
+                "descricao" => $request->descricao
+            ]);
+
+            session()->flash('mensagem', "Item alterado com sucesso.");
+            session()->flash('resultado', true);
+
+        } catch(\Exception $exception) 
+        {
+            session()->flash('mensagem', $exception->getMessage());
+            session()->flash('resultado', null);
+        }
+
+        return redirect()->route('servicos.index');
     }
 
     public function destroy(Servico $servico)
